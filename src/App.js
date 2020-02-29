e48a6140c85b4aca9e47b292fe45c5defe2d90df
bevincass <peteraugros@gmail.com>
2020-02-29 11:44:43 -0700

    cleaned up console.logs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import InfoForm from './components/InfoForm'
 import ResultsBox from './components/ResultsBox'
 
 const currencyKey = process.env.REACT_APP_CURRENCY_KEY
-console.log(currencyKey)
 
 class App extends React.Component {
 
@@ -33,10 +32,6 @@ class App extends React.Component {
 
     getCurrency = (e) => {
         e.preventDefault();
-
-        console.log("currency amount: ", this.state.currencyAmount)
-        console.log("currency to: ", this.state.currencyTo)
-        console.log("currency from: ", this.state.currencyFrom)
         
         const queryURL = `https://currency-converter5.p.rapidapi.com/currency/historical/2018-02-09?format=json&to=${this.state.currencyTo}&from=${this.state.currencyFrom}&amount=1`;
         // const queryURL = `http://data.fixer.io/api/latest?&source=USD&access_key=${currencyKey}` working;
@@ -67,27 +62,22 @@ class App extends React.Component {
       case "EUR":
         let EUR = this.state.APIresponse.data.rates.EUR.rate
         this.setState({currencyRate: EUR })
-        console.log("Rate from switch statement", EUR)
         break;
       case "JPY":
         let JPY = this.state.APIresponse.data.rates.JPY.rate
         this.setState({currencyRate: JPY })
-        console.log("Rate from switch statement", JPY)
         break;
       case "USD":
         let USD = this.state.APIresponse.data.rates.USD.rate
         this.setState({currencyRate: USD })
-        console.log("Rate from switch statement", USD)
         break;
       case "AUD":
         let AUD = this.state.APIresponse.data.rates.AUD.rate
         this.setState({currencyRate: AUD })
-        console.log("Rate from switch statement", AUD)
         break;
       case "GBP":
         let GBP = this.state.APIresponse.data.rates.GBP.rate
         this.setState({currencyRate: GBP })
-        console.log("Rate from switch statement", GBP)
         break;
         default:
     }
@@ -100,7 +90,6 @@ class App extends React.Component {
             let amount = parseInt(this.state.currencyAmount)
             let rate = this.state.currencyRate
             let result = amount * rate
-            console.log("result from calculate rate", result, "rate: ", rate, "amount: ", amount)
             this.formatResult(result)
             }
     }
@@ -109,14 +98,13 @@ class App extends React.Component {
         let decimal = result.toFixed(2);
         let formattedResult = decimal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         this.setState({finalResult: formattedResult})
-        console.log("I am the finalResult living in state", this.state.finalResult)
     }
 
     handleCurrencyFrom = e => {
         this.setState({
             currencyFrom: e.target.value
         }) 
-        console.log("currency from: ", this.state.currencyFrom) 
+      
     }
 
     handleCurrencyTo = e => {
